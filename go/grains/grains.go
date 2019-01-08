f1d0d8837cf2770fe5a4e563c4afde622719bf87
Eric Lehner <ericj@buffalo.edu>
2019-01-07 21:50:08 -0500

    Testing math.Pow solution (greater than 10 times slower)

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,6 +1,9 @@
 package grains
 
-import "errors"
+import (
+	"errors"
+	"math"
+)
 
 // Square takes a number representing the a square on teh chessboard from 1-64
 // and returns the number of grains on it.
@@ -11,14 +14,10 @@ func Square(square int) (uint64, error) {
 	if square > 64 {
 		return 0, errors.New("The squares end at 64; any number above that is invalid")
 	}
-	// Bit shifting is nice, but won't get us the zero-th power of two
-	if square == 1 {
-		return 1, nil
-	}
 
 	// In this case, we're only dealing with powers of two, which can be handled by shifting
 	// the bit over one.
-	return uint64(2 << uint(square-2)), nil
+	return uint64(math.Pow(2, float64(square-1))), nil
 }
 
 // Total returns the total count of all squares on the board added together
